48c29fe3572440be4e493cf4be3afe76b859743d
Faiz Ahmed <faiz227@proton.me>
2025-03-23 17:02:21 +0530

    Changed backend link to render. removed port

--- client/Frontend/src/apiConfig.jsx
+++ client/Frontend/src/apiConfig.jsx
@@ -1,5 +1,5 @@
 // src/apiConfig.js
 // const API_BASE_URL = 'http://localhost:8080';
-const API_BASE_URL = 'https://ss-backend-voto.onrender.com:8080';
+const API_BASE_URL = 'https://ss-backend-voto.onrender.com';
 
 export default API_BASE_URL;
